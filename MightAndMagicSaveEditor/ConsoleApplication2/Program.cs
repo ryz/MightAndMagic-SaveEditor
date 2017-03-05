95baec205dc2acd551d83ef3f041aa5a15c8f7c4
Steve Haßenpflug <ryzawy@gmail.com>
2017-03-05 23:49:38 +0100

    Identified a few more parameters (Sex, Alignment, Race) and it's now possible to change the XP of the first character

--- MightAndMagicSaveEditor/ConsoleApplication2/Program.cs
+++ MightAndMagicSaveEditor/ConsoleApplication2/Program.cs
@@ -27,42 +27,55 @@ namespace MightAndMagicSaveEditor
             bool isNewNameValid = false;
 
             var magicByte = new byte[1];
-            var unknownSection1 = new byte[4];
+
+            var sex = new byte[1];
+            var alignment = new byte[1];
+            var race = new byte[1];
+
+            var unknownSection1 = new byte[1];
 
             var characterClassNum = new byte[1];
-            var offsetOfCharacterClassChunkInFile = nameChunk.Length + magicByte.Length + unknownSection1.Length;
+            var offsetOfCharacterClassChunkInFile = nameChunk.Length + magicByte.Length + sex.Length + alignment.Length + race.Length + unknownSection1.Length;
 
 
-            // Stats
+            // Stats Chunk
             var statsAll = new byte[14]; 
-
-            var statsIntellect1 = new byte[1];
-            var statsIntellect2 = new byte[1];
-            var statsMight1 = new byte[1];
-            var statsMight2 = new byte[1];
-            var statsPersonality1 = new byte[1];
-            var statsPersonality2 = new byte[1];
-            var statsEndurance1 = new byte[1];
-            var statsEndurance2 = new byte[1];
-            var statsSpeed1 = new byte[1];
-            var statsSpeed2 = new byte[1];
-            var statsAccuracy1 = new byte[1];
-            var statsAccuracy2 = new byte[1];
-            var statsLuck1 = new byte[1];
-            var statsLuck2 = new byte[1];
 
             var characterLevel1 = new byte[1];
             var characterLevel2 = new byte[1];
 
-            var experience1 = new byte[1];
-            var experience2 = new byte[2];
+            var age = new byte[1]; // Offset 37=0x25
+
+            var unknownByte2 = new byte[1];  // Offset 38=0x26
+
+            // XP, stored as ushort/UInt16
+            var experience = new byte[2]; // Offset 39=0x27
+            var offsetExperienceChunkInFile = nameChunk.Length + magicByte.Length + sex.Length + alignment.Length + race.Length + unknownSection1.Length + characterClassNum.Length + statsAll.Length + characterLevel1.Length + characterLevel2.Length + age.Length + unknownByte2.Length;
 
 
-            ParseCharacter(stream, nameChunk, characterClassNum, statsAll, characterLevel1, experience1);
+            ParseCharacter(stream, nameChunk, sex, alignment, race, characterClassNum, statsAll, characterLevel1, experience);
+
+            byte statsIntellect1 = statsAll[0];
+            byte statsIntellect2 = statsAll[1];
+            byte statsMight1 = statsAll[2];
+            byte statsMight2 = statsAll[3];
+            byte statsPersonality1 = statsAll[4];
+            byte statsPersonality2 = statsAll[5];
+            byte statsEndurance1 = statsAll[6];
+            byte statsEndurance2 = statsAll[7];
+            byte statsSpeed1 = statsAll[8];
+            byte statsSpeed2 = statsAll[9];
+            byte statsAccuracy1 = statsAll[10];
+            byte statsAccuracy2 = statsAll[11];
+            byte statsLuck1 = statsAll[12];
+            byte statsLuck2 = statsAll[13];
+
 
             //ask and get new name from input and save it into a byte array
             Console.Write("\nEnter a new Name: ");
             string nameInput = Console.ReadLine().ToUpper();
+
+            // Truncate input above 15 characters
             nameInput = nameInput.Substring(0, Math.Min(15, nameInput.Length));
 
             // Check that the name contains only uppercase latin characters and numerals from 0-9
@@ -103,6 +116,10 @@ namespace MightAndMagicSaveEditor
             Console.Write("\nEnter a new class (1-5): ");
             byte classInput = Byte.Parse(Console.ReadLine());
 
+            Console.Write("\nEnter new XP value (0-9999): ");
+            ushort newXP = UInt16.Parse(Console.ReadLine());
+            experience = BitConverter.GetBytes(newXP);
+
             //write it back to the file
 
             //name
@@ -112,7 +129,6 @@ namespace MightAndMagicSaveEditor
                Console.ReadLine();
                stream.Seek(offsetOfNameChunkInFile, SeekOrigin.Begin);
                stream.Write(nameChunk, 0, nameChunk.Length);
-               Console.WriteLine("New name: Manfred");
             }
 
             //class
@@ -120,57 +136,130 @@ namespace MightAndMagicSaveEditor
             Console.ReadLine();
             stream.Seek(offsetOfCharacterClassChunkInFile, SeekOrigin.Begin);
             stream.WriteByte(classInput);
-            Console.WriteLine("New Class: ARCHER " + "(03)");
+
+            //exp
+            Console.WriteLine("\nWriting new XP value back to " + FILE_NAME + ". Are you sure?");
+            Console.ReadLine();
+            stream.Seek(offsetExperienceChunkInFile, SeekOrigin.Begin);
+            stream.Write(experience, 0, experience.Length);
+
+
 
             Console.ReadLine();
          }
       }
-      public static void ParseCharacter(FileStream _stream, byte[] _name, byte[] _characterClassNum, byte[] _statsAll, byte[] _level, byte[] _exp)
+      public static void ParseCharacter(FileStream _stream, byte[] _name, byte[] _sex, byte[] _alignment, byte[] _race, byte[] _characterClassNum, byte[] _statsAll, byte[] _level, byte[] _exp)
       {
-         //read dat shit 0x0 - 0xE
-         _stream.Read(_name, 0, _name.Length);
-
          Console.WriteLine("Reading first Character...\n");
 
-         //encode the byte array to a string so that we can debug it
-         var nameString = Encoding.Default.GetString(_name);
-         Console.WriteLine("Name: " + nameString + " (Length: " + nameString.Length + ")");
+         // Character Name 0x0 - 0xE
+         _stream.Read(_name, 0, _name.Length);
+
+         //encode and print the byte array to a string so that we can debug it
+         Console.WriteLine("Name: " + Encoding.Default.GetString(_name) + " (Length: " + _name.Length + ")");
          Console.WriteLine("Name Array As Hex: " + BitConverter.ToString(_name).Replace("-", " ") + " (Length: " + _name.Length + ")");
 
-         // Advance the stream 0xF, 0x10 - 0x13
-         _stream.Position += 5;
+         // Magic Byte 0xF, 
+         _stream.Position += 1;
          //_stream.Read(_magicByte, 0, _magicByte.Length);
-         //_stream.Read(_unknownSection1, 0, _unknownSection1.Length);
 
-         // Read character class - 0x14
+         // Sex 0x10
+         _stream.Read(_sex, 0, _sex.Length);
+
+         var sexS = BitConverter.ToString(_sex);
+
+         switch (sexS)
+         {
+            case "01":
+               Console.WriteLine("\nSex: Male " + "(" + sexS + ")");
+               break;
+            case "02":
+               Console.WriteLine("\nSex: Female " + "(" + sexS + ")");
+               break;
+            default:
+               throw new Exception("\nUnknown sex: " + sexS);
+         }
+
+         // Alignment 0x11
+         _stream.Read(_alignment, 0, _alignment.Length);
+
+         var alignmentS = BitConverter.ToString(_alignment);
+
+         switch (alignmentS)
+         {
+            case "01":
+               Console.WriteLine("Alignment: Good " + "(" + alignmentS + ")");
+               break;
+            case "02":
+               Console.WriteLine("Alignment: Neutral " + "(" + alignmentS + ")");
+               break;
+            case "03":
+               Console.WriteLine("Alignment: Evil " + "(" + alignmentS + ")");
+               break;
+            default:
+               throw new Exception("\nUnknown alignment: " + alignmentS);
+
+         }
+         // Race 0x12
+         _stream.Read(_race, 0, _race.Length);
+
+         var raceS = BitConverter.ToString(_race);
+
+         switch (raceS)
+         {
+            case "01":
+               Console.WriteLine("Race: Human? " + "(" + raceS + ")");
+               break;
+            case "02":
+               Console.WriteLine("Race: 02 " + "(" + raceS + ")");
+               break;
+            case "03":
+               Console.WriteLine("Race: 03 " + "(" + raceS + ")");
+               break;
+            case "04":
+               Console.WriteLine("Race: 04 " + "(" + raceS + ")");
+               break;
+            case "05":
+               Console.WriteLine("Race: 05 " + "(" + raceS + ")");
+               break;
+            default:
+               throw new Exception("\nUnknown race: " + raceS);
+
+         }
+
+         // Unknown 0x13
+         //_stream.Read(_unknownSection1, 0, _unknownSection1.Length);
+         _stream.Position += 1;
+
+         // Character Class - 0x14
          _stream.Read(_characterClassNum, 0, _characterClassNum.Length);
          var characterClassString = BitConverter.ToString(_characterClassNum);
 
          switch (characterClassString)
          {
             case "01":
-               Console.WriteLine("\nClass: KNIGHT " + "(" + characterClassString + ")");
+               Console.WriteLine("Class: KNIGHT " + "(" + characterClassString + ")");
                break;
             case "02":
-               Console.WriteLine("\nClass: PALADIN " + "(" + characterClassString + ")");
+               Console.WriteLine("Class: PALADIN " + "(" + characterClassString + ")");
                break;
             case "03":
-               Console.WriteLine("\nClass: ARCHER " + "(" + characterClassString + ")");
+               Console.WriteLine("Class: ARCHER " + "(" + characterClassString + ")");
                break;
             case "04":
-               Console.WriteLine("\nClass: CLERIC " + "(" + characterClassString + ")");
+               Console.WriteLine("Class: CLERIC " + "(" + characterClassString + ")");
                break;
             case "05":
-               Console.WriteLine("\nClass: SORCERER " + "(" + characterClassString + ")");
+               Console.WriteLine("Class: SORCERER " + "(" + characterClassString + ")");
                break;
             case "06":
-               Console.WriteLine("\nClass: ROBBER " + "(" + characterClassString + ")");
+               Console.WriteLine("Class: ROBBER " + "(" + characterClassString + ")");
                break;
             default:
-               throw new Exception("\nUnknown class: " + characterClassString);
+               throw new Exception("Unknown class: " + characterClassString);
          }
 
-         //Read stats - 0x15 - 0x22
+         // Stats - 0x15 - 0x22
          _stream.Read(_statsAll, 0, _statsAll.Length);
 
          Console.WriteLine("Stats (I/M/P/E/S/A/L): " + BitConverter.ToString(_statsAll).Replace("-", " ") + " (Length: " + _statsAll.Length + ")");
@@ -190,7 +279,7 @@ namespace MightAndMagicSaveEditor
          // Luck
 
 
-         // Read level - 0x23 - 0x24
+         // Level - 0x23 - 0x24
          _stream.Read(_level, 0, _level.Length);
          Console.WriteLine("Level: " + BitConverter.ToString(_level).Replace("-", " ") + " (Length: " + _level.Length + ")");
 
@@ -199,9 +288,9 @@ namespace MightAndMagicSaveEditor
          // Advance the stream - 0x25 - 0x26
          _stream.Position += 2;
 
-         // Read Experience - 0x27 - 0x28
+         // Experience - Stored as a little-endian ushort 0x27 - 0x28
          _stream.Read(_exp, 0, _exp.Length);
-         Console.WriteLine("Experience: " + BitConverter.ToString(_exp).Replace("-", " ") + " (Length: " + _exp.Length + ")");
+         Console.WriteLine("Experience : " + BitConverter.ToInt16(_exp, 0) + " [" + BitConverter.ToString(_exp).Replace("-", " ") + "]" + " (ushort, Length: " + _exp.Length + ")");
 
          // Advance the stream - 0x29 - 0x2C
          _stream.Position += 4;
