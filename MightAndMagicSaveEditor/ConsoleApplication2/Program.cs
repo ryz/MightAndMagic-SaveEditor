d4298b11a1a24f72dbbb0eb507c7353539b51d7e
Steve Haßenpflug <ryzawy@gmail.com>
2017-03-11 12:56:06 +0100

    Fix: alignment and race had wrong offsets; Added more editable values, cleanup

--- MightAndMagicSaveEditor/ConsoleApplication2/Program.cs
+++ MightAndMagicSaveEditor/ConsoleApplication2/Program.cs
@@ -17,7 +17,7 @@ namespace MightAndMagicSaveEditor
          string FILE_NAME = "ROSTER.DTA";
 
          Console.WriteLine("Might and Magic 1 Save Game Editor (v0.1) by ryz\n");
-         Console.WriteLine("Opening " + FILE_NAME + "...");
+         Console.WriteLine($"Opening {FILE_NAME}...");
 
          if (File.Exists(FILE_NAME))
          {
@@ -33,19 +33,24 @@ namespace MightAndMagicSaveEditor
 
                // Character Name
                var nameChunk = new byte[15]; // Offset 0=0x0
-               var offsetName = 0;
+               var nameOffset = 0;
                bool isNewNameValid = false;
 
                var unknownChunk1 = new byte[1]; // Offset 15=0xF
 
                var sexChunk = new byte[1]; // Offset 16=0x10
-               var alignmentChunk = new byte[1]; // Offset 17=0x11
-               var raceChunk = new byte[1]; // Offset 18=0x12
+               var sexOffset = 16;
 
-               var unknownChunk2 = new byte[1]; // Offset 19=0x13
+               var unknownChunk2 = new byte[1]; // Offset 17=0x11
+
+               var alignmentChunk = new byte[1]; // Offset 18=0x12
+               var alignmentOffset = 18;
+
+               var raceChunk = new byte[1]; // Offset 19=0x13
+               var raceOffset = 19;
 
                var classChunk = new byte[1]; // Offset 20=0x14
-               var offsetClass = 20;
+               var classOffset = 20;
 
                // Stats, there are seven statistics for each character, two bytes each.
                var statsChunk = new byte[14]; // Offset 21=0x15
@@ -74,7 +79,7 @@ namespace MightAndMagicSaveEditor
 
                // XP, stored as UInt24
                var xpChunk = new byte[3]; // Offset 39=0x27
-               var offsetXp = 39;
+               var xpOffset = 39;
 
                var unknownChunk4 = new byte[3]; // Offset 41=0x29
 
@@ -83,6 +88,7 @@ namespace MightAndMagicSaveEditor
                var unknownChunk5 = new byte[2]; // Offset 47=0x2F
 
                var gemsChunk = new byte[2]; // Offset 49=0x31
+               var gemsOffset = 49;
 
                var healthCurrentChunk = new byte[2]; // Offset 51=0x33
                var unknownChunkA = new byte[2]; // Offset 53
@@ -90,6 +96,7 @@ namespace MightAndMagicSaveEditor
                var healthMaxChunk = new byte[2]; // Offset 55
 
                var goldChunk = new byte[3];  // Offset 57=0x39
+               var goldOffset = 57;
 
                var unknownChunk7 = new byte[1]; // Offset 58=0x3A
 
@@ -115,13 +122,15 @@ namespace MightAndMagicSaveEditor
 
                   Console.WriteLine($"Reading Character #{i + 1} at Offset {characterOffset[i]}...\n");
 
-                  ParseCharacter(stream, nameChunk, sexChunk, alignmentChunk, raceChunk, classChunk, statsChunk, levelChunk1, xpChunk, gemsChunk, healthCurrentChunk, healthMaxChunk, goldChunk, armorClassChunk, foodChunk, conditionChunk, equippedWeaponChunk, equippedGearChunk, inventoryChunk, characterSlotChunk);
+                  ParseCharacter(stream, nameChunk, sexChunk, alignmentChunk, raceChunk, classChunk, statsChunk, levelChunk1, ageChunk, xpChunk, gemsChunk, healthCurrentChunk, healthMaxChunk, goldChunk, armorClassChunk, foodChunk, conditionChunk, equippedWeaponChunk, equippedGearChunk, inventoryChunk, characterSlotChunk);
 
                   // do work on the chunks
 
                   ModifyNameChunk(nameChunk, ref isNewNameValid);
 
                   ModifyChunkUInt8(sexChunk, "Sex", 1, 2);
+                  ModifyChunkUInt8(alignmentChunk, "Alignment", 1, 3);
+                  ModifyChunkUInt8(raceChunk, "Race", 1, 5);
                   ModifyChunkUInt8(classChunk, "Class", 1, 5);
                   ModifyChunkUInt24(xpChunk, "XP");
                   ModifyChunkUInt16(gemsChunk, "Gems");
@@ -131,14 +140,16 @@ namespace MightAndMagicSaveEditor
 
                   if (isNewNameValid)
                   {
-                     WriteChunk(stream, "Name", nameChunk, offsetName, FILE_NAME);
+                     WriteChunk(stream, "Name", nameChunk, nameOffset, FILE_NAME);
                   }
 
-                  WriteChunk(stream, "Sex", sexChunk, 16, FILE_NAME);
-                  WriteChunk(stream, "Class", classChunk, offsetClass, FILE_NAME);
-                  WriteChunk(stream, "XP", xpChunk, offsetXp, FILE_NAME);
-                  WriteChunk(stream, "Gems", gemsChunk, 49, FILE_NAME);
-                  WriteChunk(stream, "Gold", goldChunk, 57, FILE_NAME);
+                  WriteChunk(stream, "Sex", sexChunk, sexOffset, FILE_NAME);
+                  WriteChunk(stream, "Alignment", alignmentChunk, alignmentOffset, FILE_NAME);
+                  WriteChunk(stream, "Race", raceChunk, raceOffset, FILE_NAME);
+                  WriteChunk(stream, "Class", classChunk, classOffset, FILE_NAME);
+                  WriteChunk(stream, "XP", xpChunk, xpOffset, FILE_NAME);
+                  WriteChunk(stream, "Gems", gemsChunk, gemsOffset, FILE_NAME);
+                  WriteChunk(stream, "Gold", goldChunk, goldOffset, FILE_NAME);
 
                   Console.WriteLine($"\nCharacter #{i + 1} done!\n");
                }
@@ -246,25 +257,26 @@ namespace MightAndMagicSaveEditor
       }
 
       public static void ParseCharacter(
-         FileStream _stream, 
-         byte[] _name, 
-         byte[] _sex, 
-         byte[] _alignment, 
-         byte[] _race, 
-         byte[] _characterClassNum, 
-         byte[] _statsChunk, 
-         byte[] _level, 
-         byte[] _exp, 
-         byte[] _gems, 
-         byte[] _health, 
+         FileStream _stream,
+         byte[] _name,
+         byte[] _sex,
+         byte[] _alignment,
+         byte[] _race,
+         byte[] _class,
+         byte[] _statsChunk,
+         byte[] _level,
+         byte[] _age,
+         byte[] _exp,
+         byte[] _gems,
+         byte[] _health,
          byte[] _healthMax,
-         byte[] _gold, 
-         byte[] _armorClass, 
-         byte[] _food, 
-         byte[] _condition, 
-         byte[] _equippedWeapon, 
-         byte[] _equipment, 
-         byte[] _inventory, 
+         byte[] _gold,
+         byte[] _armorClass,
+         byte[] _food,
+         byte[] _condition,
+         byte[] _equippedWeapon,
+         byte[] _equipment,
+         byte[] _inventory,
          byte[] _characterSlot
          )
 
@@ -274,9 +286,7 @@ namespace MightAndMagicSaveEditor
          _stream.Read(_name, 0, _name.Length);
 
          //encode and print the byte array to a string so that we can debug it
-         Console.WriteLine($"Name: " + Encoding.Default.GetString(_name) + " (Length: " + _name.Length + ")");
-         
-         //Console.WriteLine("Name Array As Hex: " + BitConverter.ToString(_name).Replace("-", " ") + " (Length: " + _name.Length + ")");
+         Console.WriteLine($"Name: {Encoding.Default.GetString(_name)} (Length: {_name.Length})");
 
          // Magic Byte 0xF, 
          _stream.Position += 1;
@@ -298,7 +308,10 @@ namespace MightAndMagicSaveEditor
                throw new Exception($"\nUnknown Sex: {sexS}");
          }
 
-         // Alignment 0x11
+         // Unknown 0x11
+         _stream.Position += 1;
+
+         // Alignment 0x12
          _stream.Read(_alignment, 0, _alignment.Length);
 
          var alignmentS = BitConverter.ToString(_alignment);
@@ -315,10 +328,12 @@ namespace MightAndMagicSaveEditor
                Console.WriteLine($"Alignment: Evil ({alignmentS})");
                break;
             default:
-               throw new Exception($"\nUnknown alignment: {alignmentS}");
+               //throw new Exception($"Unknown alignment: {alignmentS}");
+               Console.WriteLine($"Unknown alignment: {alignmentS}");
+               break;
 
          }
-         // Race 0x12
+         // Race 0x13
          _stream.Read(_race, 0, _race.Length);
 
          var raceS = BitConverter.ToString(_race);
@@ -345,71 +360,67 @@ namespace MightAndMagicSaveEditor
 
          }
 
-         // Unknown 0x13
-         //_stream.Read(_unknownSection1, 0, _unknownSection1.Length);
-         _stream.Position += 1;
-
          // Character Class - 0x14
-         _stream.Read(_characterClassNum, 0, _characterClassNum.Length);
-         var characterClassString = BitConverter.ToString(_characterClassNum);
+         _stream.Read(_class, 0, _class.Length);
 
-         switch (characterClassString)
+         var classS = BitConverter.ToString(_class);
+
+         switch (classS)
          {
             case "01":
-               Console.WriteLine($"Class: KNIGHT ({characterClassString})");
+               Console.WriteLine($"Class: KNIGHT ({classS})");
                break;
             case "02":
-               Console.WriteLine($"Class: PALADIN ({characterClassString})");
+               Console.WriteLine($"Class: PALADIN ({classS})");
                break;
             case "03":
-               Console.WriteLine($"Class: ARCHER ({characterClassString})");
+               Console.WriteLine($"Class: ARCHER ({classS})");
                break;
             case "04":
-               Console.WriteLine($"Class: CLERIC ({characterClassString})");
+               Console.WriteLine($"Class: CLERIC ({classS})");
                break;
             case "05":
-               Console.WriteLine($"Class: SORCERER ({characterClassString})");
+               Console.WriteLine($"Class: SORCERER ({classS})");
                break;
             case "06":
-               Console.WriteLine($"Class: ROBBER ({characterClassString})");
+               Console.WriteLine($"Class: ROBBER ({classS})");
                break;
             default:
-               throw new Exception($"Unknown class: {characterClassString}");
+               throw new Exception($"Unknown class: {classS}");
          }
 
          // Stats - 0x15 - 0x22
          _stream.Read(_statsChunk, 0, _statsChunk.Length);
 
-         Console.WriteLine("Stats (I/M/P/E/S/A/L): " + BitConverter.ToString(_statsChunk).Replace("-", " ") + " (Length: " + _statsChunk.Length + ")");
+         Console.WriteLine($"Stats (I/M/P/E/S/A/L): {BitConverter.ToString(_statsChunk).Replace("-", " ")} (Length: {_statsChunk.Length})");
 
          // Intellect
-
          // Might
-
          // Perception
-
          // Endurance
-
          // Speed
-
          // Accuracy
-
          // Luck
-
 
          // Level - 0x23 - 0x24
          _stream.Read(_level, 0, _level.Length);
-         Console.WriteLine("Level: " + BitConverter.ToString(_level).Replace("-", " ") + " (Length: " + _level.Length + ")");
+         int levelNum = _level[0];
+         Console.WriteLine($"Level: {levelNum} [{BitConverter.ToString(_level)}]");
 
          _stream.Position += 1;
 
-         // Advance the stream - 0x25 - 0x26
-         _stream.Position += 2;
+         // Age Offset 37=0x25
+         _stream.Read(_age, 0, _age.Length);
+         int ageNum = _age[0];
+         Console.WriteLine($"Age: {ageNum} [{BitConverter.ToString(_age)}]");
+
+         // Advance the stream - 0x26
+         _stream.Position += 1;
 
          // Experience - Stored as a little-endian UInt24 0x27 - 0x29
          _stream.Read(_exp, 0, _exp.Length);
          int expNum = (_exp[2] << 16) | (_exp[1] << 8) | _exp[0];
-         Console.WriteLine("Experience : " + expNum + " [" + BitConverter.ToString(_exp).Replace("-", " ") + "]" + " (UInt24, Length: " + _exp.Length + ")");
+         Console.WriteLine($"Experience : {expNum} [{BitConverter.ToString(_exp).Replace("-", " ")}] (UInt24, Length: {_exp.Length})");
 
 
          // Advance the stream - 0x2A - 0x2C
@@ -423,25 +434,25 @@ namespace MightAndMagicSaveEditor
 
          // Gems - Stored as a little-endian ushort  0x31 - 0x32
          _stream.Read(_gems, 0, _gems.Length);
-         Console.WriteLine("Gems: " + BitConverter.ToInt16(_gems, 0) + " [" + BitConverter.ToString(_gems) + "]" + " (ushort, Length: " + _gems.Length + ")");
+         Console.WriteLine($"Gems: {BitConverter.ToInt16(_gems, 0)} [{BitConverter.ToString(_gems)}] (ushort, Length: {_gems.Length})");
 
          // Health Points - 0x33 - 0x34
          _stream.Read(_health, 0, _health.Length);
-         Console.WriteLine("Health: " + BitConverter.ToUInt16(_health, 0) + " [" + BitConverter.ToString(_health) + "]" + " (ushort, Length: " + _health.Length + ")");
+         Console.WriteLine($"Health: " + BitConverter.ToUInt16(_health, 0) + " [" + BitConverter.ToString(_health) + "]" + " (ushort, Length: " + _health.Length + ")");
 
          // ?? 0x37 - 0x38
          _stream.Position += 2;
 
          // Max HP - 0x35 - 0x36
          _stream.Read(_healthMax, 0, _healthMax.Length);
-         Console.WriteLine("Max Health: " + BitConverter.ToUInt16(_healthMax, 0) + " [" + BitConverter.ToString(_healthMax) + "]" + " (ushort, Length: " + _healthMax.Length + ")");
+         Console.WriteLine($"Max Health: {BitConverter.ToUInt16(_healthMax, 0)} [{BitConverter.ToString(_healthMax)}] (ushort, Length: {_healthMax.Length})");
 
 
 
          // Gold - 0x39 - 0x3B
          _stream.Read(_gold, 0, _gold.Length);
          int goldNum = (_gold[2] << 16) | (_gold[1] << 8) | _gold[0];
-         Console.WriteLine("Gold : " + goldNum + " [" + BitConverter.ToString(_gold).Replace("-", " ") + "]" + " (UInt24, Length: " + _gold.Length + ")");
+         Console.WriteLine($"Gold: {goldNum} [{BitConverter.ToString(_gold).Replace("-", " ")}] (UInt24, Length: {_gold.Length})");
 
 
          // Advance the stream 0x3C
@@ -449,34 +460,61 @@ namespace MightAndMagicSaveEditor
 
          // Armor Class - 0x3D
          _stream.Read(_armorClass, 0, _armorClass.Length);
-         Console.WriteLine("AC: " + BitConverter.ToString(_armorClass));
+         int acNum = _armorClass[0];
+         Console.WriteLine($"AC: {acNum} [{BitConverter.ToString(_armorClass)}]");
 
          // Food - 0x3E
          _stream.Read(_food, 0, _food.Length);
-         Console.WriteLine("Food: " + BitConverter.ToString(_food));
+         int foodNum = _food[0];
+         Console.WriteLine($"Food: {foodNum} [{BitConverter.ToString(_food)}]");
 
          // Condition - 0x3F
          _stream.Read(_condition, 0, _condition.Length);
-         Console.WriteLine("Condition: " + BitConverter.ToString(_condition));
+
+         var conditionS = BitConverter.ToString(_condition);
+
+         switch(conditionS)
+         {
+            case "00":
+               Console.WriteLine($"Condition: Good ({conditionS})");
+               break;
+            case "01":
+               Console.WriteLine($"Condition: 01 ({conditionS})");
+               break;
+            case "02":
+               Console.WriteLine($"Condition: 02 ({conditionS})");
+               break;
+            case "03":
+               Console.WriteLine($"Condition: 03 ({conditionS})");
+               break;
+            case "04":
+               Console.WriteLine($"Condition: 04 ({conditionS})");
+               break;
+            case "05":
+               Console.WriteLine($"Condition: 05 ({conditionS})");
+               break;
+            default:
+               throw new Exception($"Unknown condition: {conditionS}");
+         }
 
          // Equipped Weapon - 0x40
          _stream.Read(_equippedWeapon, 0, _equippedWeapon.Length);
-         Console.WriteLine("Equipped Weapon: " + BitConverter.ToString(_equippedWeapon));
+         Console.WriteLine($"Equipped Weapon: {BitConverter.ToString(_equippedWeapon)}");
 
          // Other equipment - 0x41 - 0x45
          _stream.Read(_equipment, 0, _equipment.Length);
-         Console.WriteLine("Equipment: " + BitConverter.ToString(_equipment));
+         Console.WriteLine($"Equipment: {BitConverter.ToString(_equipment)}");
 
          // Inventory - 0x46 - 0x4B
          _stream.Read(_inventory, 0, _inventory.Length);
-         Console.WriteLine("Inventory: " + BitConverter.ToString(_inventory));
+         Console.WriteLine($"Inventory: {BitConverter.ToString(_inventory)}");
 
          // Advance the stream - 0x4C - 0x7D
          _stream.Position += 50;
 
          // Character slot number - 0x7E
          _stream.Read(_characterSlot, 0, _characterSlot.Length);
-         Console.WriteLine("Character Slot: " + BitConverter.ToString(_characterSlot));
+         Console.WriteLine($"Character Slot: {BitConverter.ToString(_characterSlot)}");
       }
 
 
